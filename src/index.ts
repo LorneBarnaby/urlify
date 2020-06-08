8bfce04514f77f3c50a19a11ca1a75970c4f80cf
Lorne Barnaby <lorne123barnaby@gmail.com>
2020-06-08 22:33:34 +0100

    added url encoding

--- src/index.ts
+++ src/index.ts
@@ -14,10 +14,10 @@ export const generateQuery = (parameters : object) : QueryString => {
 
     const params = Object(parameters);
     if(keys.length > 0){
-        query.text += `${keys[0]}=${params[keys[0]]}`;
+        query.text += `${keys[0]}=${encodeURIComponent(params[keys[0]])}`;
         keys.shift();
         for(const param of keys){
-            query.text += `&${param}=${params[param]}`;
+            query.text += `&${param}=${encodeURIComponent(params[param])}`;
         }
     }
     return query;
